e20d2288d60d099b79d780fb89b0d8d10ec22f7b
MatHenriquez <matias.henriquez.dev@gmail.com>
2025-01-11 16:58:16 -0300

    feat: add API and client route constants

---
+++ src/app/constants/routes/api-routes.ts
@@ -0,0 +1,15 @@
+export const API_ROUTES = {
+    AUTH: {
+        LOGIN: '/Auth/Login',
+        USER_TOKEN: '/Auth/UserToken',
+    },
+    BALANCES: '/Balances',
+    TRANSACTION: {
+        ACCOUNT_CREATION: '/Transaction/AccountCreation',
+        PAYMENT: '/Transaction/Payment',
+        TEST_FUND: '/Transaction/TestFund',
+        BALANCE: '/Transaction/Balance',
+    },
+    WALLETS: '/Wallets',
+    USER: '/User',
+};
---
+++ src/app/constants/routes/front-routes.ts
@@ -0,0 +1,9 @@
+export const CLIENT_ROUTES = {
+    AUTH: {
+        LOGIN: '/auth/login',
+        SIGNUP: '/auth/signup',
+    },
+    DASHBOARD: '/dashboard',
+    HOME: '/',
+    TRANSACTIONS: '/transactions',
+}

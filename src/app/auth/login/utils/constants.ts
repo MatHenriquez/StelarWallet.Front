048d5e750626dcee2ccb56190632bf7e48aa51b4
MatHenriquez <matias.henriquez.dev@gmail.com>
2025-01-11 16:57:49 -0300

    refactor: update LoginInitialValues to use IBaseUser model

--- src/app/auth/login/utils/constants.ts
+++ src/app/auth/login/utils/constants.ts
@@ -1,7 +1,7 @@
-import { ILoginUser } from '@/models/login-user';
+import { IBaseUser } from '@/models/base-user';
 import { IErrorResponseBody } from '@/utils/interfaces/api-base-response';
 
-export const LoginInitialValues: ILoginUser = {
+export const LoginInitialValues: IBaseUser = {
   email: '',
   password: '',
 };
